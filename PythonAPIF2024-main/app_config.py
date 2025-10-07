5106daea9fe18d2b971efb9654c2338e8b5cb1ae
ndalbah <158243596+ndalbah@users.noreply.github.com>
2025-10-07 16:26:47 -0400

    Update app_config.py
    removed url and token secret for security

--- PythonAPIF2024-main/app_config.py
+++ PythonAPIF2024-main/app_config.py
@@ -1,7 +1,8 @@
-CONST_MONGO_URL = "mongodb+srv://UND3RTAK3R:Bourne123@cluster0.5udx2.mongodb.net/Undertook?retryWrites=true&w=majority&appName=Cluster0"
+CONST_MONGO_URL = "(url)"
 CONST_DATABASE = "Undertook"
 CONST_USER_COLLECTION = "Users"
 CONST_TASK_COLLECTION = "Task" 
 
 JWT_EXPIRATION = 86400*30
-TOKEN_SECRET = "ls3fall"
+
+TOKEN_SECRET = "(secret)"
